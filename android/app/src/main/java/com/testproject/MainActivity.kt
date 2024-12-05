9fbc0699b2f9614fdc708cde8659b957c736cf30
Maksim Attsetski <maks2001maks2001@gmail.com>
2024-12-05 13:37:44 +0300

    rename the ptoject

--- android/app/src/main/java/com/testproject/MainActivity.kt
+++ android/app/src/main/java/com/testproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.testproject
+package com.MasyaMusic
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "testProject"
+  override fun getMainComponentName(): String = "MasyaMusic"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/testproject/MainApplication.kt
+++ android/app/src/main/java/com/testproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.testproject
+package com.MasyaMusic
 
 import android.app.Application
 import com.facebook.react.PackageList
